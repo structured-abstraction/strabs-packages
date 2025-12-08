d5649202bda2b620a81016b0fa90f61b804414a9
Lakin Wecker <lakin@wecker.ca>
2025-12-08 12:21:13 -0700

    fix: Turn this into a workspace project

--- devenv.nix
+++ devenv.nix
@@ -4,10 +4,15 @@
   languages.python = {
     enable = true;
     package = pkgs.python312;
-    uv.enable = true;
+    uv = {
+      enable = true;
+      sync = {
+        enable = true;
+        allPackages = true;
+        allExtras = true;
+      };
+    };
   };
 
-  packages = [
-    pkgs.python312Packages.invoke
-  ];
+  packages = [ pkgs.python312Packages.invoke ];
 }
